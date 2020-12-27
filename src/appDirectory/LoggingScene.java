a6f240d545a751d15423ddc0fdd3648e324e238c
mzieba <mzieba>
2020-12-27 19:37:09 +0100

    split LogginScene into 2 classes, Label builder does not work

--- src/appDirectory/LoggingScene.java
+++ src/appDirectory/LoggingScene.java
@@ -3,26 +3,80 @@ package appDirectory;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class LoggingScene {
-    Button siema;
+    Label loginLabel;
+    Label repeatPasswordLabel;
+    Label passwordLabel;
+
+    TextField loginTextField = new TextField();
+    PasswordField passwordField = new PasswordField();
+    PasswordField repeatPasswordField = new PasswordField();
+
+    Button setCredentials;
+    Button returnButton;
+
     Scene loggingScene;
+    Scene credentialsScene;
+    Scene currentScene;
 
-    LoggingScene(){
-        siema = new Button();
-        siema.setText("siema, press me");
-        siema.setOnAction(e -> System.out.println("siema"));
+    VBox loggingLayout;
+    VBox credentialsLayout;
 
-        VBox loggingLayout = new VBox();
+    LoggingScene(Stage stage){
+
+        //setting credentials button
+        setCredentials = new Button();
+        setCredentials.setText("sign in");
+        setCredentials.setOnAction(e -> {
+            System.out.println("credentials");
+            loggingLayout = null;
+            currentScene = credentialsScene;
+            stage.setScene(setLoggingScene());
+        });
+
+        //return to main logging scene button
+        returnButton = new Button("return");
+        returnButton.setOnAction(e -> {
+            System.out.println("return");
+            credentialsLayout = null;
+            currentScene = loggingScene;
+            stage.setScene(setLoggingScene());
+        });
+
+        loggingLayout = new VBox(20);
         loggingLayout.setAlignment(Pos.CENTER);
-        loggingLayout.getChildren().add(siema);
+        loggingLayout.getChildren().addAll(
+                loginLabel = new LabelBuilder().makePretty("Arial", 15, 30, 30),
+                loginTextField,
+                passwordLabel = new LabelBuilder().makePretty("Arial", 15, 30, 30),
+                passwordField,
+                setCredentials
+        );
 
+        credentialsLayout = new VBox(20);
+        credentialsLayout.setAlignment(Pos.CENTER);
+        credentialsLayout.getChildren().addAll(
+                loginLabel = new LabelBuilder().makePretty("Arial", 15, 30, 30),
+                loginTextField,
+                passwordLabel = new LabelBuilder().makePretty("Arial", 15, 30, 30),
+                passwordField,
+                repeatPasswordLabel = new LabelBuilder().makePretty("Arial", 15, 30, 30),
+                repeatPasswordField,
+                returnButton
+        );
+
+        credentialsScene = new Scene(credentialsLayout, 600, 600);
         loggingScene = new Scene(loggingLayout, 600, 600);
+        currentScene = credentialsScene;
     }
 
     public Scene setLoggingScene() {
-        return loggingScene;
+        return currentScene;
     }
 }
--- src/appDirectory/Main.java
+++ src/appDirectory/Main.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -14,12 +16,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Stage mainWindow = primaryStage;
+       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new LoggingScene().setLoggingScene());
-        primaryStage.show();
+        Stage mainWindow = primaryStage;
+        LoggingScene loggingScene = new LoggingScene(mainWindow);
+        mainWindow.setTitle("freeweed");
+        mainWindow.setScene(loggingScene.setLoggingScene());
+        mainWindow.setScene();
+        mainWindow.show();
     }
 
 
