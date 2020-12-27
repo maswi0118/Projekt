85716e9a4e2657236f88388496fafe052d39884c
mzieba <mzieba>
2020-12-27 19:36:10 +0100

    nie działa, trzeba inaczej rozwiazac tworzenie kustomowych labeli/ innych gownien

---
+++ src/appDirectory/LabelBuilder.java
@@ -0,0 +1,23 @@
+package appDirectory;
+
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class LabelBuilder extends Label {
+    Label label;
+    LabelBuilder(){
+        label = new Label();
+    }
+    public Label makePretty(String font,
+                            double size,
+                            double width,
+                            double height) {
+
+        label.setFont(new Font(font, size));
+        label.setMaxHeight(height);
+        label.setMaxHeight(width);
+        label.setTextFill(Color.LIGHTGREEN);
+        return label;
+    }
+}
