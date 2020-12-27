a7a93a3fa37e7b4923768a4a2ebd8daf3a0189fa
mzieba <mzieba>
2020-12-27 16:18:06 +0100

    added logging scene skelet

---
+++ src/appDirectory/LoggingScene.java
@@ -0,0 +1,28 @@
+package appDirectory;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+public class LoggingScene {
+    Button siema;
+    Scene loggingScene;
+
+    LoggingScene(){
+        siema = new Button();
+        siema.setText("siema, press me");
+        siema.setOnAction(e -> System.out.println("siema"));
+
+        VBox loggingLayout = new VBox();
+        loggingLayout.setAlignment(Pos.CENTER);
+        loggingLayout.getChildren().add(siema);
+
+        loggingScene = new Scene(loggingLayout, 600, 600);
+    }
+
+    public Scene setLoggingScene() {
+        return loggingScene;
+    }
+}
